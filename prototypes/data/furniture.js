bac4372eeeac05f3347c63d8f343f814c106ec42
Liliana Weimer <lilianaweimer11@gmail.com>
2020-05-23 14:02:51 -0600

    Add initial furniture array

---
+++ prototypes/data/furniture.js
@@ -0,0 +1,21 @@
+const furniture = [
+    {'antique': 
+        ['bed', 'bureau', 'chair', 'clock', 'console table', 'mini table', 'phone', 'vanity', 'wardrobe']
+    }, 
+    {'imperial': 
+        ['bed', 'chest', 'low table', 'partition', 'decorative shelves']
+    }, 
+    {'rattan': 
+        ['armchair', 'bed', 'end table', 'low table', 'stool', 'table lamp', 'towel basket', 'vanity', 'wardrobe', 'waste bin']
+    }, 
+    {'bamboo': 
+        ['basket', 'bench', 'candleholder', 'doll', 'drum', 'floor lamp', 'flooring', 'wall', 'lattice fence', 'lunch box', 'noodle slide', 'partition', 'shelf', 'lamp', 'speaker', 'sphere', 'stool', 'stopblock', 'wall decoration', 'wand', 'basket pack', 'dark rug', 'green-leaf pile', 'light rug', 'pan flute', 'steamer-basket set']
+    }, 
+    {'zodiac': 
+        ['aries rocking chair', 'aquarius urn', 'cancer table', 'capricorn ornament', 'gemini chest', 'gemini closet', 'leo sculpture', 'libra scale', 'pisces lamp', 'sagittarius arrow', 'scorpio lamp', 'taurus bathtub', 'virgo harp']
+    }, 
+    {'rococo': 
+        ['bed', 'candlestick', 'chair', 'clock', 'dresser', 'lamp', 'shelf', 'sofa', 'table', 'vanity', 'wardrobe', 'wall', 'floor']
+    }];
+
+    module.exports = furniture;
